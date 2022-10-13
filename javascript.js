b9eaf1266d64a08ca849cae9378673bfdbcfebd0
JapSir <japsircode@gmail.com>
2022-10-14 00:10:40 +0200

    createbasic user interface

--- javascript.js
+++ javascript.js
@@ -1,4 +1,38 @@
-game()
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+
+const divResults = document.querySelector("#results");
+const divPointsUser = document.querySelector("#pointsUser");
+const divPointsComp = document.querySelector("#pointsComp");
+const divWinner = document.querySelector("#winner");
+
+let userPointsCounter = 0;
+let compPointsCounter = 0;
+
+
+
+function checkWinner() {
+    if (userPointsCounter === 5) {
+        divWinner.innerText = "You have won, congratulations!";
+        userPointsCounter = 0;
+        compPointsCounter = 0;
+    } else if (compPointsCounter === 5){
+        divWinner.innerText = "You have lost, please try harder!";
+        compPointsCounter = 0;
+        userPointsCounter = 0;
+
+    }
+}
+function clearWinners() {
+    divWinner.innerText = ""
+}
+
+function updatePoints() {
+    divPointsComp.innerText = compPointsCounter;
+    divPointsUser.innerText = userPointsCounter;
+}
+
 
 //gets a number from 1 to three, returns each number as a rock, paper or scissors
 function getComputerChoice() {
@@ -8,89 +42,73 @@ function getComputerChoice() {
     } else if (num === 2) {
         return "paper"
     } else {
-        return "scissor"
-    }
-}
-function getPlayerChoice() {
-    userInput = prompt("Rock, Paper or Scissors?").toLowerCase();
-    //turns scissor into scissors so both are correct
-    if (userInput == "scissor") {
         return "scissors"
     }
-    //checks user input is valid
-    if (userInput === "rock" || userInput === "paper" || userInput == "scissors") {
-        return userInput
-    //makes sure it calls itsself again if the user didnt give correct input
-    } else {
-        alert("Please give correct input")
-        getPlayerChoice();
-    }
+    
 }
-
 //gives 1 point for a win, -1 for a lose, 0 for a tie
 function playRound(playerSelection, computerSelection) {
     //returns message for userInput = rock
     if (playerSelection === "rock") {
         if (computerSelection === "paper") {
-            console.log("You lose! Paper beats Rock");
-            return -1
+            divResults.innerText = "You lose! Paper beats Rock";
+            compPointsCounter++;
+            return
         } else if (computerSelection === "scissors") {
-            console.log("You win! Rock beats Scissors");
-            return 1
+            divResults.innerText = "You win! Rock beats Scissors";
+            userPointsCounter++;
+            return
         } else {
-            console.log("Tie!");
-            return 0
+            divResults.innerText = "Tie!";
+            return 
         }
     }
     //returns message if userInput = paper
     if (playerSelection === "paper") {
         if (computerSelection === "scissors") {
-            console.log("You lose! Scissors beats Paper")
-            return -1
+            divResults.innerText = "You lose! Scissors beats Paper"
+            compPointsCounter++;
+            return
         } else if (computerSelection === "rock") {
-            console.log("You win! Paper beats Rock")
-            return 1
+            divResults.innerText = "You win! Paper beats Rock"
+            userPointsCounter++;
+            return
         } else {
-            console.log("Tie!");
-            return 0
+            divResults.innerText = "Tie!";
+            return
         }
     }
     //returns message if userInput = scissors
     if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
-            console.log("You lose! Rock beats Scissors");
-            return -1
+            divResults.innerText = "You lose! Rock beats Scissors";
+            compPointsCounter++;
+            return
         } else if (computerSelection === "paper") {
-            console.log("You win! Scissors beats paper");
-            return 1
+            divResults.innerText = "You win! Scissors beats paper";
+            userPointsCounter++;
+            return
         } else {
-            console.log("Tie!");
-            return 0
+            divResults.innerText = "Tie!";
+            return
         }
     }
     return "Error, something went wrong"
 }
-
-//adds the points to let points, returns winner or tie 
-function game() {
-    //sets points to 0 at start of the game
-    let points = 0;
-
-    //loops 5 times: does 5 games.
-    for (let i = 0;  i < 5; i++) {
-        let num = playRound(getPlayerChoice(), getComputerChoice());
-        points += num;
-    }
-    if (points < 0) {
-        console.log("You are the loser!")
-    } else if (points > 0) {
-        console.log("You are the winner!")
-    } else if (points === 0) {
-        console.log("Its a tie!")
-    } else {
-        console.log("Sorry, something went wrong")
-    }
+function simulateGame(userChoice, computerChoice) {
+    playRound(userChoice, computerChoice);
+    clearWinners();
+    checkWinner();
+    updatePoints()
 }
 
 
-
+rockButton.addEventListener("click", () => {
+    simulateGame("rock", getComputerChoice());
+});
+paperButton.addEventListener("click", () => {
+    simulateGame("paper", getComputerChoice());
+});
+scissorsButton.addEventListener("click", () => {
+    simulateGame("scissors", getComputerChoice());
+});
